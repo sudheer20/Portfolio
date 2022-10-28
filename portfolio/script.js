3f93018c13ca7926933e82ae4b53937f109ed6d4
Sudheer Dadala <46629803+sudheer20@users.noreply.github.com>
2022-10-28 00:27:21 -0700

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+/* <a href="www.linkedin.com/in/sudheerdadala">LinkedIn </a> */
+// href="https://github.com/sudheer20"
+/* <a href="mailto:sudheerdadala1997@gmail.com"> */
+
+function openlinkedin(){
+    window.open('https://www.linkedin.com/in/sudheerdadala','_blank');
+}
+
+function openmail(){
+    window.location.href = 'mailto:sudheerdadala1997@gmail.com';
+}
